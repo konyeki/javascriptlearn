39cbbea459498748b544d4238196e32616843204
konyeki <konye.evans@yahoo.com>
2024-06-28 09:42:58 -0400

    javascript added

---
+++ SampleFolder/scope.js
@@ -0,0 +1,34 @@
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+    //Block scope
+    var blockVar = "I'm a block-scoped var"; 
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
+
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+
+}
+
+//Global scope
+//console.log(globalVar);
+//console.log(globalLet);
+//console.log(globalConst);
+
+//function
+show();
+console.log(functionVar);
+console.log(functionLet);
+console.log(functionConst);
+
+//Block scope
+//console.log(blockVar);
+//console.log(blockLet);
+//console.log(blockConst);
