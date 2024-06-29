9b0162b30474c67ad11fd77917174290dcf811f5
konyeki <konye.evans@yahoo.com>
2024-06-29 04:23:56 -0400

    updated javascriptf

--- groceryTracker/grocer_tracker.js
+++ groceryTracker/grocer_tracker.js
@@ -3,11 +3,11 @@ let grocer_2;
 let grocer_3; 
 
 function computeTotalAmount(){
-    grocer_1 = parseFloat(document.getElementById("glocery_1").value);
+    grocer_1 = parseFloat(document.getElementById("grocery_1").value);
     grocer_2 = parseFloat(document.getElementById("grocery_2").value);
     grocer_3 = parseFloat(document.getElementById('grocery_3').value);
 
-    totalAmount = grocer_1 + grocer_2 + grocer_3;
+    let totalAmount = grocer_1 + grocer_2 + grocer_3;
 
     document.getElementById("result").innerText = `The total amount for the grocery is: ${totalAmount}`;
 }
