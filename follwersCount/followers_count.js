22004bacb8b898f6438ac8816c9ac24cd9f05d7e
konyeki <konye.evans@yahoo.com>
2024-06-29 06:13:22 -0400

    user counter

---
+++ follwersCount/followers_count.js
@@ -0,0 +1,24 @@
+let count = 0; 
+
+//create a function to display the counter
+
+function displayCount(){
+    document.getElementById("countDisplay").innerHTML=count;
+}
+
+//creat a function to increase the counter
+function increaseCount(){
+    count++; //increment counter by 1
+    displayCount();
+    checkCountValue();
+}
+
+//Function to check the value of the count and trigger alerts
+function checkCountValue(){
+    if(count === 10){
+        alert("Your instagram post gained 10 followers! Congratulations!");        
+    }else if (count === 20){
+        alert("Your instagram post gained 20 followers! Keep it up!");
+    }
+}
+
