21cb6fa0acc500f685316fe8ef7be05c7ab50c30
konyeki <konye.evans@yahoo.com>
2024-07-03 08:08:24 -0400

    user information html

---
+++ employeeDetails/employee_details.js
@@ -0,0 +1,14 @@
+const employees = [
+    {id: 1, name:'Evans Konye', age: 24, department:'IT', salary: 50000},
+    {id: 2, name: 'Julius Makungu', age: 34, department: 'HR', salary: 45000},
+    {id: 3, name: 'Jane Wairimu', age: 43, department: 'Finance', salary: 60000}
+];
+
+function displayEmployees(){
+    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join(' ');
+    document.getElementById(employeesDetails).innerHTML = totalEmployees;
+}
+
+function calculateTotalSalaries(){
+
+}
