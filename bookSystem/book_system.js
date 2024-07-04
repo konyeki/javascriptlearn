110fcf73a0e838bcb8fd5336f85362bc79e1b9f1
konyeki <konye.evans@yahoo.com>
2024-07-04 02:46:00 -0400

    show book function

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -32,7 +32,11 @@ function addBook(){
 
 function showbooks(){
     const booksDiv = books.map((book, index) => `<h1> book Number: ${index + 1}</h1>
-    <p><strong>Book Name: </strong>${book.name}</p>
-    <p><strong>Author Name:</strong>${book.authorName}</p>
+        <p><strong>Book Name: </strong>${book.name}</p>
+        <p><strong>Author Name:</strong>${book.authorName}</p>
+        <p><strong>Book Description:</strong>${book.bookDescription}</p>
+        <p><strong>No. of Pages:</strong>${book.pagesNumber}</p>
     `);
+
+    document.getElementById('books').innerHTML = booksDiv.join(' ');
 }
